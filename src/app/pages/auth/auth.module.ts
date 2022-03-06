399733f5fd85c1cadbf184fd66553f9398ec9c0f
vamidi <valencio_masaki16@live.nl>
2022-03-06 10:34:42 +0100

    release 2020.1.7f6 - fix invite component

--- src/app/pages/auth/auth.module.ts
+++ src/app/pages/auth/auth.module.ts
@@ -2,7 +2,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { NbFirebasePasswordStrategy, NbFirebasePasswordStrategyOptions } from '@nebular/firebase-auth';
-import { NbAlertModule, NbButtonModule, NbCheckboxModule, NbIconModule, NbInputModule } from '@nebular/theme';
+import {
+	NbAlertModule,
+	NbButtonModule, NbCardModule,
+	NbCheckboxModule,
+	NbIconModule,
+	NbInputModule,
+	NbLayoutModule,
+} from '@nebular/theme';
 import { getDeepFromObject, NbAuthJWTToken, NbAuthModule } from '@nebular/auth';
 import { NbAuthSocialLink } from '@nebular/auth/auth.options';
 import { NgxFirebaseLoginComponent } from './firebase-login.component';
@@ -34,6 +41,8 @@ const NB_MODULES = [
 	NbIconModule,
 	NbCheckboxModule,
 	NbButtonModule,
+	NbLayoutModule,
+	NbCardModule,
 ];
 
 @NgModule({
